bf22644a987e365ca8f2703daaba38e4eba8105c
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-10-27 23:51:22 +0300

    complete redux works

--- myWorks/src/pages/Component_A.js
+++ myWorks/src/pages/Component_A.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import {useSelector, useDispatch} from "react-redux"
 
 
 const Component_A = (props) => {
-
+    const myCounter = useSelector(globalState => globalState.counter);
+    const dispatch = useDispatch();
 
     return (
         <View style={{ backgroundColor: '#eceff1', flex: 1 }}>
             <Text>Component_A</Text>
+            <Text style={{fontSize: 50}}>Counter: {myCounter}</Text>
+            <Button
+                title="increase"
+                onPress={() => dispatch({type: "INCREASE_COUNTER"})}
+            />
+            <Text></Text>
+            <Button
+                title="decrease"
+                onPress={() => dispatch({type: "DECREASE_COUNTER"})}
+            />
   
         </View>
     );
--- myWorks/src/pages/Component_B.js
+++ myWorks/src/pages/Component_B.js
@@ -1,15 +1,17 @@
   
 import React from 'react';
 import { View, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux"
+
 
 const Component_B = (props) => {
-    const ourCounter = useSelector(onur => onur.counter)
+
+    const ourCounter = useSelector(myGlobalState => myGlobalState.counter)
 
     return (
         <View style={{ backgroundColor: '#bdbdbd', flex: 1 }}>
             <Text>Component_B</Text>
-            <Text style={{ fontSize: 50 }}>Counter: {ourCounter}</Text>
+            <Text style={{fontSize: 50}}>Counter: {ourCounter}</Text>
 
         </View>
     );
